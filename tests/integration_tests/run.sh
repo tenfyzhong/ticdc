cc3904aa83578b9031601c0edaa499a0de640998
tenfyzhong <tenfy@tenfy.cn>
2025-06-23 21:05:09 +0800

    chore(integration_tests): Export CUR variable for integration test execution
    - The `CUR` variable, which points to the current directory of the `run.sh` script, is now exported.
    - This ensures that any sub-processes or nested scripts called during integration tests can correctly access and utilize the `CUR` path, preventing potential issues with path resolution in child shells.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>
    ################################################################################
    # type: fix/feat/build/chroe/ci/docs/style/refactor/perf/test/BREAK
    # <type>(<scope>): title
    #
    # body
    #
    # footer
    #
    ################################################################################

--- tests/integration_tests/run.sh
+++ tests/integration_tests/run.sh
@@ -7,7 +7,7 @@
 set -eu
 
 OUT_DIR=/tmp/tidb_cdc_test
-CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+export CUR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 export PATH=$PATH:$CUR/_utils:$CUR/../bin:$CUR/../../bin:$CUR/../../scripts/bin
 export TICDC_NEWARCH=true
 
