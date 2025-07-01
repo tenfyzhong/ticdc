e84585a278c5aa4d1bb9fedad832a1e772edff83
tenfyzhong <tenfy@tenfy.cn>
2025-07-01 15:36:46 +0800

    test(integration_tests): Refactor collation compatibility test setup
    - Query start_ts after CDC server is running to ensure a valid timestamp is obtained.
    - Use create_changefeed helper function for clearer changefeed creation.
    
    Signed-off-by: tenfyzhong <tenfy@tenfy.cn>

--- tests/integration_tests/ci_collation_compatibility/run.sh
+++ tests/integration_tests/ci_collation_compatibility/run.sh
@@ -22,8 +22,8 @@ function run() {
 
 	cd $WORK_DIR
 
-	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
 	run_cdc_server --workdir $WORK_DIR --binary $CDC_BINARY
+	start_ts=$(run_cdc_cli_tso_query ${UP_PD_HOST_1} ${UP_PD_PORT_1})
 
 	TOPIC_NAME="ticdc-ci-collation-compatibility-test-$RANDOM"
 	case $SINK_TYPE in
@@ -36,7 +36,7 @@ function run() {
 	*) SINK_URI="mysql://normal:123456@127.0.0.1:3306/?safe-mode=true" ;;
 	esac
 
-	run_cdc_cli changefeed create --start-ts=$start_ts --sink-uri="$SINK_URI"
+	create_changefeed --start-ts=$start_ts --sink-uri="$SINK_URI"
 
 	case $SINK_TYPE in
 	kafka) run_kafka_consumer $WORK_DIR "kafka://127.0.0.1:9092/$TOPIC_NAME?protocol=open-protocol&partition-num=4&version=${KAFKA_VERSION}&max-message-bytes=10485760" ;;
