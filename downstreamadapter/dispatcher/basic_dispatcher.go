5d40b72ff89ff2abeead55ce6f9a4b4dc6ac77f9
tenfyzhong <tenfy@tenfy.cn>
2025-11-16 17:09:56 +0800

    Revert "*: add some logs (#3142)"
    This reverts commit f555bd7bd41db0c82da31ef2f32e29da0b5a3649.

--- downstreamadapter/dispatcher/basic_dispatcher.go
+++ downstreamadapter/dispatcher/basic_dispatcher.go
@@ -28,7 +28,6 @@ import (
 	commonEvent "github.com/pingcap/ticdc/pkg/common/event"
 	"github.com/pingcap/ticdc/pkg/errors"
 	"github.com/pingcap/ticdc/pkg/sink/util"
-	"github.com/pingcap/ticdc/pkg/spanz"
 	"go.uber.org/zap"
 )
 
@@ -394,19 +393,10 @@ func (d *BasicDispatcher) handleEvents(dispatcherEvents []DispatcherEvent, wakeC
 		}
 
 		if event.GetType() == commonEvent.TypeDMLEvent {
-			dml := event.(*commonEvent.DMLEvent)
-			for i, rowType := range dml.RowTypes {
-				rowKey := dml.RowKeys[i]
-				log.Info("dispatcher receive dml row",
-					zap.Stringer("dispatcher", d.id),
-					zap.Int64("mode", d.mode),
-					zap.String("eventType", commonEvent.TypeToString(event.GetType())),
-					zap.Uint64("commitTs", event.GetCommitTs()),
-					zap.Uint64("startTs", event.GetStartTs()),
-					zap.Int("rowType", int(rowType)),
-					zap.String("hexKey", spanz.HexKey(rowKey)),
-					zap.Any("rawKey", rowKey))
-			}
+			log.Info("dispatcher receive all event",
+				zap.Stringer("dispatcher", d.id), zap.Int64("mode", d.mode),
+				zap.String("eventType", commonEvent.TypeToString(dispatcherEvent.Event.GetType())),
+				zap.Any("event", dispatcherEvent.Event))
 		}
 
 		// only when we receive the first event, we can regard the dispatcher begin syncing data
